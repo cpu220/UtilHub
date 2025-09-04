fdf95b53018f9c5fd0f4a439ad8bef07e5c34c28
Belial <102864814@qq.com>
2025-09-04 13:49:09 +0800

    feat: 功能2： 创建字帖格子

--- git.sh
+++ git.sh
@@ -1,2 +1,2 @@
-git add . && git commit -m "chore: 初步实现字体展示&动画循环"
+git add . && git commit -m "feat: 功能2： 创建字帖格子"
 # 保存git记录并提交，使用符合conventional commit标准的格式，方便版本对比和问题排查
--- src/pages/charsheet/custom/index.tsx
+++ src/pages/charsheet/custom/index.tsx
@@ -1,6 +1,6 @@
- 
+
 import React, { useEffect, useState } from 'react';
-import { Form, message, Input, Button, Card, Typography } from 'antd';
+import { Form, Row, Col, message, Input, Button, Card, Typography } from 'antd';
 import type { FormProps } from 'antd';
 import { renderHanziInContainer, cleanupHanziWriter } from '@/utils';
 import styles from './index.less';
@@ -10,16 +10,23 @@ const { Title, Paragraph } = Typography;
 /**
  * 自定义字帖生成页面
  */
-const CustomCharsheetPage : React.FC = () => {
+const CustomCharsheetPage: React.FC = () => {
 
   const [form] = Form.useForm();
   const [characters, setCharacters] = useState('');
+  const [gridData, setGridData] = useState<any[]>([]);
 
-  useEffect(()=>{
-    createGrid(8,16)
-  },[characters])
+  useEffect(() => {
+    // 调用 renderGrid 方法来渲染网格
+    const grid = createGrid(8, 16); 
+    setGridData(grid);
+    // 清理函数
+    return () => {
+      cleanupHanziWriter('grid-container');
+    };
+  }, [characters])
 
-  const createGrid = (x:number,y:number)=>{
+  const createGrid = (x: number, y: number) => {
     const grid = [];
     for (let i = 0; i < y; i++) {
       const row = [];
@@ -36,9 +43,57 @@ const CustomCharsheetPage : React.FC = () => {
     return grid;
   }
 
+  /**
+   * 创建单个方格项
+   * @param item 网格项
+   * @returns React节点
+   */
+  const createBlockItem = (item: any) => {
+    return (
+      <div 
+      id={`grid-item-${item.x}-${item.y}`} 
+      key={item.x + item.y} 
+      className={styles['grid-item']}
+      style={{
+        width: '100px',
+        height: '100px',
+        border: '1px solid #000',
+  
+      }}>
+        {item.character}
+      </div>
+    )
+  }
+
+  /**
+   * 根据createGrid返回的二维数组在页面上创建方格
+   * @param grid 二维数组网格数据
+   */
+  const renderGrid = (grid: any[]) => {
+    // 存储网格数据到state
+    const result = [];
+    for (let i = 0; i < grid.length; i++) {
+      const row = [];
+      for (let j = 0; j < grid[i].length; j++) {
+        const item = grid[i][j];
+        const blockItem = createBlockItem(item);
+        row.push(blockItem);
+      }
+      const rowDOM = (
+        <div id={`grid-row-${i}`} key={i} className={styles['grid-row']}> {row}</div>
+      )
+      result.push(rowDOM);
+    }
+
+    return result;
+  }
+
   return (
     <div style={{ padding: '24px' }}>
-      
+
+      <div id="grid-container">
+        {renderGrid(gridData)}
+      </div>
     </div>
   );
 }
